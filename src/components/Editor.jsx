845d22c355c7a8778c9dddd4fea43c91572f2a59
Abubakar Siddiqui <abubakarsiddiqui866@gmail.com>
2023-04-07 06:05:17 +0530

    initial commit

---
+++ src/components/Editor.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+export default function Editor({ currentNote, updateNote, updateTitle }) {
+  const [selectedTab, setSelectedTab] = useState("write");
+
+  const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
+
+  return (
+    <section className="pane editor">
+      <input
+        type="text"
+        id="title"
+        className="app-main-note-title"
+        value={currentNote.title}
+        onChange={(event) => updateTitle(event.target.value)}
+        autoFocus
+      />
+      <ReactMde
+        value={currentNote.body}
+        onChange={updateNote}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        minEditorHeight={40}
+        heightUnits="vh"
+      />
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{currentNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">
+          {currentNote.body}
+        </ReactMarkdown>
+      </div>
+    </section>
+  );
+}
